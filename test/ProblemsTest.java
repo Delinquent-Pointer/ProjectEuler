151b43a26bcc1a87c8747ea7df7a0810193bed99
JimmyNull <yyang13@ewu.edu>
2024-01-13 15:09:12 -0800

    Ver 0.0.3

--- test/ProblemsTest.java
+++ test/ProblemsTest.java
@@ -1,5 +1,4 @@
 import static org.junit.Assert.*;
-
 import org.junit.Test;
 
 public class ProblemsTest {
@@ -10,5 +9,10 @@ public class ProblemsTest {
     assertEquals("Answer: 233168", result);
   }
 
-
+  @Test
+  public void testEvenFibNum(){
+    Problems problem = new Problems();
+    String result = problem.evenFibNum();
+    assertEquals("You need go back to 210!",result,"Answer: 4613732");
+  }
 }
