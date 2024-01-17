fe6cdb04191cea54ce28e581f029748ff5146aa4
JimmyNull <yyang13@ewu.edu>
2024-01-17 12:58:45 -0800

    Ver 0.0.5

--- test/ProblemsTest.java
+++ test/ProblemsTest.java
@@ -8,21 +8,25 @@ public class ProblemsTest {
   @Test
   public void testM3or5() {
     Problems problem = new Problems();
-    String result = problem.m3or5();
-    assertEquals("Answer: 233168", result);
+    String actual = problem.m3or5();
+    assertEquals("Final answer isn't correct.", "Answer: 233168", actual);
   }
 
   @Test // this need to be here to be tested
   public void testEvenFibNum(){
     Problems problem = new Problems();
-    String result = problem.evenFibNum();
-    assertEquals("Final answer isn't correct.",result,"Answer: 4613732");
+    String actual = problem.evenFibNum();
+    assertEquals("Final answer isn't correct.","Answer: 4613732", actual);
   }
 
   public void testIsPrime(){
     Problems problem = new Problems();
 
   }
+
+  /**
+   * Class notes 1
+   */
 
 //  @Test
   public void testInformation(){
@@ -34,5 +38,21 @@ public class ProblemsTest {
 //    assertNull();
 //    assertNotNull();
 //    fail("message here");
+    // Timeout should be in every time for infinite loop
+
   }
+
+  /**
+   * Class notes 2
+   */
+  @Test
+  public void TestDateMonthYear(){
+    int year = 2024;
+    int month = 12;
+    int day = 32; // depends on month check what month then days associate aka leap year too
+    assertFalse("Invalid year: Year cannot be less than zero", year < 0);
+    assertFalse("Invalid month: month cannot be less than 1 or bigger than 12", month < 0 || month > 12);
+    assertFalse("Invalid month: day cannot be less than 1 or bigger than 12", day < 0 || day > 31);
+  }
+
 }
