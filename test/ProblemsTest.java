f768ff3f24d98572d8d825a6c3280f0eaee9e47c
JimmyNull <yyang13@ewu.edu>
2024-01-26 10:03:02 -0800

    Ver 0.1.0 Notes

--- test/ProblemsTest.java
+++ test/ProblemsTest.java
@@ -4,19 +4,29 @@ import org.junit.*; //import org.junit.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 import org.junit.runners.MethodSorters;
+import org.junit.runners.*;
+import org.junit.runner.*;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+  Problems.class
+})
 
 @FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class ProblemsTest {
 
-  @Test(timeout = 100)
-  public void testM3or5() {
+  private static final int TIMEOUT = 500;
+
+  @Test(timeout = TIMEOUT)
+  public void testM3or5_compare_final_answer() {
     Problems problem = new Problems();
     String actual = problem.m3or5();
     assertEquals("Final answer isn't correct.", "Answer: 233168", actual);
   }
 
-  @Test(timeout = 100)// this need to be here to be tested
-  public void testEvenFibNum() {
+  @Test(timeout = TIMEOUT)// this need to be here to be tested
+  public void testEvenFibNum_compare_final_answer() {
     Problems problem = new Problems();
     String actual = problem.evenFibNum();
     assertEquals("Final answer isn't correct.", "Answer: 4613732", actual);
@@ -32,7 +42,7 @@ public class ProblemsTest {
    * Java documentation example
    */
 
-  @Test(timeout = 5)
+  @Test(timeout = TIMEOUT)
   public void palindrome_WithPalindromeNumber_ShouldReturnTrue() {
     Problems problem = new Problems();
 
@@ -41,7 +51,7 @@ public class ProblemsTest {
     assertTrue("This should be a palindrome", result);
   }
 
-  @Test(timeout = 5)
+  @Test(timeout = TIMEOUT)
   public void palindrome_WithNonPalindromeNumber_ShouldReturnFalse() {
     Problems problem = new Problems();
 
@@ -50,7 +60,7 @@ public class ProblemsTest {
     assertFalse("This should NOT be a palindrome", result);
   }
 
-  @Test(timeout = 5)
+  @Test(timeout = TIMEOUT)
   public void palindrome_WithNegativeNumber_ShouldReturnFalse() {
     Problems problem = new Problems();
 
@@ -60,10 +70,16 @@ public class ProblemsTest {
   }
 
   @ParameterizedTest
-  @ValueSource(ints = {98089, 161})
-  void palindrome_ShouldReturnTrue(int number) {
+  @ValueSource(ints = {98089, 161, 0})
+  public void palindrome_ShouldReturnTrue(int number) {
     Problems result = new Problems();
     assertTrue("This should be a palindrome", result.isPalindrome(number));
   }
 
+  @ParameterizedTest
+  @ValueSource(ints = {-999, 1234})
+  void palindrome_ShouldReturnFalse(int number) {
+    Problems result = new Problems();
+    assertFalse("This should NOT be a palindrome", result.isPalindrome(number));
+  }
 }
