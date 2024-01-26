4b4186fd6a2087ed70553e3dea0e88620d2f46ba
JimmyNull <yyang13@ewu.edu>
2024-01-25 19:57:58 -0800

    Ver 0.0.8 Notes

--- test/ProblemsTest.java
+++ test/ProblemsTest.java
@@ -3,46 +3,61 @@
 import static org.junit.Assert.*;
 
 import org.junit.*;
+
 //import org.junit.Test;
 
 
 public class ProblemsTest {
 
-  @Test
+  @Test(timeout = 100)
   public void testM3or5() {
     Problems problem = new Problems();
     String actual = problem.m3or5();
     assertEquals("Final answer isn't correct.", "Answer: 233168", actual);
   }
 
-  @Test // this need to be here to be tested
+  @Test(timeout = 100)// this need to be here to be tested
   public void testEvenFibNum() {
     Problems problem = new Problems();
     String actual = problem.evenFibNum();
     assertEquals("Final answer isn't correct.", "Answer: 4613732", actual);
   }
 
-  @Test
-  public void testIsPrime() {
-    Problems problem = new Problems();
-
-  }
+//  @Test
+//  public void testIsPrime() {
+//    Problems problem = new Problems();
+//
+//  }
 
   /**
    * Java documentation example
    */
 
-  @Test
-  public void testIsPalindrome() {
+  @Test(timeout = 5)
+  public void palindrome_WithPalindromeNumber_ShouldReturnTrue() {
     Problems problem = new Problems();
-    boolean actual = problem.isPalindrome(9009);
-    assertTrue("This should be a palindrome", actual);
-    actual = problem.isPalindrome(123);
-    assertFalse("This should NOT be a palindrome", actual);
-    actual = problem.isPalindrome(646);
-    assertTrue("This should be a palindrome", actual);
-    actual = problem.isPalindrome(-151);
-    assertFalse("This should NOT be a palindrome", actual);
+
+    boolean result = problem.isPalindrome(636);
+
+    assertTrue("This should be a palindrome", result);
+  }
+
+  @Test(timeout = 5)
+  public void palindrome_WithNonPalindromeNumber_ShouldReturnFalse() {
+    Problems problem = new Problems();
+
+    boolean result = problem.isPalindrome(123);
+
+    assertFalse("This should NOT be a palindrome", result);
+  }
+
+  @Test(timeout = 5)
+  public void palindrome_WithNegativeNumber_ShouldReturnFalse() {
+    Problems problem = new Problems();
+
+    boolean result = problem.isPalindrome(-151);
+
+    assertFalse("This should NOT be a palindrome", result);
   }
 
 
